af1332067ee4deaad0458fc8ca779093bc89907d
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-12-24 11:43:43 +0300

    added

--- contacts.js
+++ contacts.js
@@ -1,1 +1,14 @@
 const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join("./db", "contacts.json");
+
+const updateContacts = async (contacts) => {
+  const data = await fs.writeFile(
+    contactsPath,
+    JSON.stringify(contacts, null, 2)
+  );
+  console.log(data);
+};
+
+updateContacts();
